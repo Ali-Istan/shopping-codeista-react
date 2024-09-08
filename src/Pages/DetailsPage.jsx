d882dd11f90d6fae12b055d4488d9e3302fe1094
Ali Istan <ali.istan@outlook.com>
2024-09-08 22:40:28 +0330

    uncomment

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useCard } from "../context/CardContext";
 import { Link, useParams } from "react-router-dom";
 
-import Loader from "../components/Loader";
+import Loader from "../Components/Loader";
 import { SiOpenproject } from "react-icons/si";
 import { IoMdPricetag } from "react-icons/io";
 import { FiArrowLeft } from "react-icons/fi";
